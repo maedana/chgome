d05154ba746bb45031281b6d9ccbea6b1ff78321
maedana <maeda.na@gmail.com>
2017-04-16 09:00:58 +0900

    chromeの履歴を標準出力に全件だすように出来たところまで

--- cmd/history.go
+++ cmd/history.go
@@ -21,24 +21,59 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
-
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 // historyCmd represents the history command
 var historyCmd = &cobra.Command{
 	Use:   "history",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "標準出力にGoogle Chromeの閲覧履歴を表示",
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("history called")
+
+		// chromeが動いてるとHistoryがロックされていて読めないのでコピーする
+		b, err := ioutil.ReadFile(viper.GetString("history_db_path"))
+		if err != nil {
+			panic(err)
+		}
+		var useDbPath = os.Getenv("HOME") + "/.chgome/History"
+		err = ioutil.WriteFile(useDbPath, b, 0744)
+		if err != nil {
+			panic(err)
+		}
+
+		db, err := sql.Open("sqlite3", useDbPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		rows, err := db.Query("select url, title from urls")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var url string
+			var title string
+			err = rows.Scan(&url, &title)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(url + "|" + title)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	},
 }
 
